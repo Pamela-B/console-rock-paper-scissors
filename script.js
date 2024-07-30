019ede6ae8d9679ee5ac82e88fb07b5187664291
Pamela Brickman <pamela.brickman@gmail.com>
2024-07-29 21:24:26 -0300

    Add playRound function

--- script.js
+++ script.js
@@ -10,9 +10,64 @@ function getComputerChoice(){
 };
 
 function getHumanChoice() {
-    const humanChoice = prompt("Choose! Rock, Paper, Scissors?").toLowerCase()
-    return humanChoice;
+    return prompt("Choose! Rock, Paper, Scissors?").toLowerCase();
 }
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice,computerChoice) {
+    if (humanChoice === computerChoice) {
+        return console.log("You both chose the same!")
+    } else {
+        switch (humanChoice) {
+            case "rock":
+                switch (computerChoice) {
+                    case "paper":
+                        console.log("You lose! Paper beats Rock");
+                        computerScore++;
+                        break;
+                    case "scissors":
+                        console.log("You win! Rock beats Scissors");
+                        humanScore++;
+                        break;
+                }
+                break;
+
+            case "paper":
+                switch (computerChoice) {
+                    case "rock":
+                        console.log("You win! Paper beats Rock");
+                        humanScore++;
+                        break;
+                    case "scissors":
+                        console.log("You lose! Scissors beat Paper");
+                        computerScore++;
+                        break;
+                }
+                break;
+
+            case "scissors":
+                switch (computerChoice) {
+                    case "paper":
+                        console.log("You win! Scissors beat Paper");
+                        humanScore++;
+                        break;
+                    case "rock":
+                        console.log("You lose! Rock beats Scissors");
+                        computerScore++;
+                        break;
+                }
+                break;
+
+            default:
+                console.log("That's not a valid choice!");
+        }
+    }
+}
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection,computerSelection);
